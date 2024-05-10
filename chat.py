708f3ca590e5c626763a440dde504945c650f3cb
Gabriel Penke <gabri.penke@gmail.com>
2024-05-09 21:49:27 -0300

    feat: first elements

--- chat.py
+++ chat.py
@@ -1,1 +1,14 @@
+# importar o flet
 import flet as ft
+
+# criar a função principal do app
+def main(pagina):
+    # cria o elemento
+    titulo = ft.Text("Penke ZAP")
+    botao_iniciar = ft.ElevatedButton("Iniciar Chat")
+
+    # adiciona o elemento na página
+    pagina.add(titulo)
+    pagina.add(botao_iniciar)
+# rodar app
+ft.app(main)
