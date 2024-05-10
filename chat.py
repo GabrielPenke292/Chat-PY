f97aa55454923ddfbb3580b9871c60257a57f453
Gabriel Penke <gabri.penke@gmail.com>
2024-05-09 21:56:28 -0300

    feat: function to inicialize chat

--- chat.py
+++ chat.py
@@ -1,14 +1,17 @@
 # importar o flet
 import flet as ft
 
+def iniciar_chat(evento):
+    print("Iniciar Chat")
+
 # criar a função principal do app
 def main(pagina):
     # cria o elemento
     titulo = ft.Text("Penke ZAP")
-    botao_iniciar = ft.ElevatedButton("Iniciar Chat")
+    botao_iniciar = ft.ElevatedButton("Iniciar Chat", on_click=iniciar_chat)
 
     # adiciona o elemento na página
     pagina.add(titulo)
     pagina.add(botao_iniciar)
 # rodar app
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER)
