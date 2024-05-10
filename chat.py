e171091861c8bb7051e5a8df77a48ba3653d372b
Gabriel Penke <gabri.penke@gmail.com>
2024-05-09 22:10:12 -0300

    feat: show dialog when click in button

--- chat.py
+++ chat.py
@@ -1,13 +1,23 @@
 # importar o flet
 import flet as ft
 
-def iniciar_chat(evento):
-    print("Iniciar Chat")
+
 
 # criar a função principal do app
 def main(pagina):
     # cria o elemento
     titulo = ft.Text("Penke ZAP")
+
+    def iniciar_chat(evento):
+        titulo_janela = ft.Text("Bem vindo ao Penke Zap")
+        campo_nome_usuario = ft.TextField(label="Escreva seu nome no site")
+        botao_entrar = ft.ElevatedButton("Entrar no chat")
+        janela = ft.AlertDialog(title=titulo_janela, content=campo_nome_usuario, actions=[botao_entrar])
+        pagina.dialog = janela
+        janela.open = True
+        pagina.update()        
+        print("Iniciar Chat")
+
     botao_iniciar = ft.ElevatedButton("Iniciar Chat", on_click=iniciar_chat)
 
     # adiciona o elemento na página
